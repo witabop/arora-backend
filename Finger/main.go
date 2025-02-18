da259dbe08cebda759f49ac974c354725a2b8b82
Jameson <108284606+witabop@users.noreply.github.com>
2025-02-17 18:39:56 -0600

    Update main.go

--- Finger/main.go
+++ Finger/main.go
@@ -41,7 +41,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// test
 	// test 2
 	//test 3
-	//test 478
+	//test 4789
 	//test 5
 	bytesResponse, err := json.Marshal(responseData)
 	if err != nil {
